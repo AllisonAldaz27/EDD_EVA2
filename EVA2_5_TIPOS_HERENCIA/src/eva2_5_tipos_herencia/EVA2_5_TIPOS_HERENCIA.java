e5e02bc5c56969fac5d9f0b46d73b4ecd0e01353
AllisonAldaz27 <124476993+AllisonAldaz27@users.noreply.github.com>
2023-03-09 09:34:10 -0600

    Add files via upload

---
+++ EVA2_5_TIPOS_HERENCIA/src/eva2_5_tipos_herencia/EVA2_5_TIPOS_HERENCIA.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_5_tipos_herencia;
+
+/**
+ *
+ * @author VOSTRO 3401
+ */
+public class EVA2_5_TIPOS_HERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    class otrasuperclase{//no se puede heredar de dos o mas clasews
+        
+    }
+}  //extends solo funciona para una unica clase
+class SuperClase{
+}
+
+    class BaseClase extends SuperClase{
+        
+    
+}
